f5d51c16fce6f6a79569eaf21e061d5ade717068
João Henrique <joao.henrique@timesharesolucoes.com.br>
2023-10-06 11:35:15 -0300

    Aplicativo Criptomoedas 5.5

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,14 @@ import 'package:aplicativo_criptomoeda/repository/moedas_favoritas_repository.da
 import 'package:aplicativo_criptomoeda/config/app_settings.dart';
 import 'package:aplicativo_criptomoeda/config/hive_configs.dart';
 import 'package:aplicativo_criptomoeda/my_app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await HiveConfigs.start();
+  await Firebase.initializeApp();
 
   runApp(
     MultiProvider(
